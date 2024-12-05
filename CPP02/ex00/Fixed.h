86928aabb8ab489945264ebc087a5d49d7d8e309
Mario Cantelli <mcantell@e3r9p1.42roma.it>
2024-12-05 16:22:01 +0100

    ex00 done, studies are require

---
+++ CPP02/ex00/Fixed.h
@@ -0,0 +1,25 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+	int					_fixedPointValue;
+	static const int	_fractionalBits = 8;
+
+	public:
+			/* Constructor */
+			Fixed();
+			/* Copy contructor */
+			Fixed( const Fixed& other );
+			/* Operator */
+			Fixed& operator=( const Fixed& other );
+			/* Destructo */
+			~Fixed();
+
+			int		getRawBits( void ) const;
+			void	setRawBits( int const raw );
+};
+
+#endif
